27dedee0d53554a4a91b31392ec59b389be0fad7
yatender <yatender@getvokal.com>
2019-02-05 09:54:33 +0530

    first commit

---
+++ src/main/java/io/codementor/gtommee/rest_tutorial/RestTutorialApplication.java
@@ -0,0 +1,14 @@
+package io.codementor.gtommee.rest_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTutorialApplication.class, args);
+	}
+
+}
+
